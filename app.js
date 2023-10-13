e84b3ba720ed6d92dd962b7eed63b28826526736
leemingoo <dwmapi@naver.com>
2023-10-13 14:26:52 +0900

    서버 기본 틀

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const fs = require('fs');
 const http = require("http");
 
+http.createServer(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/html' })
+
+
+}).listen(8080);
