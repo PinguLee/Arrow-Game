04134defb890eb5a49010362c428bac4f49efbbc
leemingoo <dwmapi@naver.com>
2023-10-13 11:17:48 +0900

    함수를 객체로 변환

--- js/script.js
+++ js/script.js
@@ -2,25 +2,23 @@ const root = document.getElementById('root');
 const keyBox = document.getElementById('key-Box');
 const countLabel = document.getElementById('count');
 const startButton = document.getElementById('start');
-const createKey = document.createElement('li');
-
-createKey.setAttribute('class', 'key');
 
 const key = {
-  1:"↑",
-  2:"→",
-  3:"↓",
-  4:"←"
+  1: "↑",
+  2: "→",
+  3: "↓",
+  4: "←"
 };
 
-function getRandomInt() {
+const getRandomOneToFour = () => {
   let min = Math.ceil(1);
   let max = Math.floor(4);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-startButton.addEventListener('click', ()=>{
+startButton.addEventListener('click', () => {
   for (let i = 0; i < 4; i++) {
-    console.log(key[getRandomInt()]);
+
+    console.log(key[getRandomOneToFour()]);
   }
 });
