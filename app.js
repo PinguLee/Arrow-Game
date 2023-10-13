4afce0fc69cfc0399153a908f6389cb3c9098583
leemingoo <dwmapi@naver.com>
2023-10-13 14:27:27 +0900

    index.html 읽어오기

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const http = require("http");
 http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'text/html' })
 
-
+  fs.readFile("./index.html", function (err, data) {
+    if (err) {
+      console.error("Error");
+    } else {
+      response.end(data);
+    }
+  });
 }).listen(8080);
