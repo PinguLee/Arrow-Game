d5adf38df2b93712051b3831d8944a7409ddb328
leemingoo <dwmapi@naver.com>
2023-10-13 14:46:30 +0900

    readFile로 index.html 읽기

--- app.js
+++ app.js
@@ -5,22 +5,27 @@ const contentType = {
   'Content-Type': 'text/html',
   'charset': 'utf-8'
 };
+let doc;
+
+fs.readFile("./index.html", (err, data) => {
+  if (err) {
+    console.error("Error");
+  } else {
+    doc = data;
+  }
+});
 
 http.createServer((request, response) => {
 
   if (request.method === 'GET' && request.url === '/') {
     response.writeHead(200, contentType);
-    response.end(data);
+    response.end(doc);
   } else {
-
+    response.writeHead(404, contentType);
+    response.end(doc);
   }
 
-  fs.readFile("./index.html", (err, data) => {
-  if (err) {
-    console.error("Error");
-  } else {
-
-  }
+  
 });
 
 });
