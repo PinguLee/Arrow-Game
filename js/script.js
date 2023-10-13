f280566c2c474f2c599cd60f014e246785c1a443
leemingoo <dwmapi@naver.com>
2023-10-13 10:49:39 +0900

    오타 수정

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const root = document.getElementById('root');
+const keyBox = document.getElementById('key-Box');
+const countLabel = document.getElementById('count');
+const startButton = document.getElementById('start');
+const createKey = document.createElement('li');
+
+createKey.setAttribute('class', 'key');
+
+const key = [1, 2, 3, 4];
