d374c345b457d3a8d34447c1210f774066b15966
leemingoo <dwmapi@naver.com>
2023-10-16 16:42:39 +0900

    css js GET을 사용해 불러오기

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const http = require('http');
 const fs = require('fs');
-const http = require("http");
 
-const contentType = {
-  'Content-Type': 'text/html',
-  'charset': 'utf-8'
+const port = 8080;
+const ContentTypeHTML = {
+  'Content-Type': 'text/html'
+};
+const ContentTypeCSS = {
+  'Content-Type': 'text/css'
+};
+const ContentTypeJavaScript = {
+  'Content-Type': 'application/javascript'
 };
 let indexDoc, hardDoc;
 
@@ -25,13 +31,29 @@ fs.readFile("./hard.html", (err, data) => {
 
 const server = http.createServer((request, response) => {
   if (request.method === 'GET' && request.url === '/') {
-    response.writeHead(200, contentType);
+    response.writeHead(200, ContentTypeHTML);
     response.end(indexDoc);
   } else if (request.method === 'GET' && request.url === '/hard') {
-    response.writeHead(200, contentType);
+    response.writeHead(200, ContentTypeHTML);
     response.end(hardDoc);
+  } else if (request.url === '/css/style.css' && request.method === 'GET') {
+    fs.readFile('./static/css/style.css', 'utf8', (err, data) => {
+      if (err) {
+        serverErrorLog();
+      }
+      response.writeHead(200, ContentTypeCSS);
+      response.end(data);
+    });
+  } else if (request.url === '/scripts/script.js' && request.method === 'GET') {
+    fs.readFile('./static/scripts/script.js', 'utf8', (err, data) => {
+      if (err) {
+        serverErrorLog();
+      }
+      response.writeHead(200, ContentTypeJavaScript);
+      response.end(data);
+    });
   } else {
-    response.writeHead(404, contentType);
+    response.writeHead(404, ContentTypeHTML);
     response.end('404 ERROR');
   }
 });
